241aed0f3abbe651e7e7daddf47f1d892ac7cfff
tj <tanmay@mail.usf.edu>
2020-01-30 12:50:42 -0500

    fixed a typo in scalegauss

--- dataset_generator/func_helper.py
+++ dataset_generator/func_helper.py
@@ -277,8 +277,8 @@ def ProcessScaleGauss3Points(vertices, highResVertMap, args):
 
 		asum = Process3DGaussOnePoint(vert, highResVertMap[a_point]["pos"], fixedAmplitude, fixedStdVec)
 		#we may need to change the formula here to fixedAmplitude - Aprimedist? we want to get the difference between the prior amplitude, but b4, it wouldve just given us the global value
-		a_prime0_sum = Process3DGaussOnePoint(vert, highResVertMap[a_prime_point]["pos"], max(0, fixedAmplitude - A0PrimeDist), fixedStdVec)
-		a_prime1_sum = Process3DGaussOnePoint(vert, highResVertMap[a_prime_point]["pos"], max(0, fixedAmplitude - A1PrimeDist), fixedStdVec)
+		a_prime0_sum = Process3DGaussOnePoint(vert, highResVertMap[a_prime0_point]["pos"], max(0, fixedAmplitude - A0PrimeDist), fixedStdVec)
+		a_prime1_sum = Process3DGaussOnePoint(vert, highResVertMap[a_prime1_point]["pos"], max(0, fixedAmplitude - A1PrimeDist), fixedStdVec)
 
 		gaussA_total = bsum + asum
 		gaussA_prime0_total = bsum + a_prime0_sum
